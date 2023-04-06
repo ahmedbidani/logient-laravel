a28ec341b1168ecd49f81cf14ba71c875e7ea5c0
bidaniahmed <bidaniahmed@gmail.com>
2023-04-06 11:27:42 +0100

    feat<links>: link creation page

---
+++ resources/views/links/create.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create Link') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    <section>
+                        <header>
+                            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                {{ __('New Link') }}
+                            </h2>
+
+                            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                                {{ __('Put the url you want to short linked it below') }}
+                            </p>
+                        </header>
+
+                        <form method="post" action="{{ route('links.store') }}" class="mt-6 space-y-6">
+                            @csrf
+
+                            <div>
+                                <x-input-label for="url" :value="__('Url')" />
+                                <x-text-input id="url" name="url" type="text"
+                                    class="mt-1 block w-full" autocomplete="url" />
+                                <x-input-error :messages="$errors->updatePassword->get('url')"
+                                    class="mt-2" />
+                            </div>
+
+                            <div class="flex items-center gap-4">
+                                <x-primary-button>{{ __('Save') }}</x-primary-button>
+
+                                @if (session('status') === 'password-updated')
+                                <p x-data="{ show: true }" x-show="show" x-transition
+                                    x-init="setTimeout(() => show = false, 2000)"
+                                    class="text-sm text-gray-600 dark:text-gray-400">{{ __('Saved.') }}</p>
+                                @endif
+                            </div>
+                        </form>
+                    </section>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
